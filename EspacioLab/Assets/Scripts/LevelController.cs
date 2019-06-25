81ddfada672bcae223689ed7a10cd29527fe88b9
luiscvillarreall <luiscvillarreall@gmail.com>
2019-06-24 21:12:11 -0500

    problemas de camara en build

---
+++ EspacioLab/Assets/Scripts/LevelController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+
+    [SerializeField] int delay = 2;
+
+  public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene("spaceLab");
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+
+
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Game Over");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
--- EspacioLab/Assets/Scripts/Player.cs
+++ EspacioLab/Assets/Scripts/Player.cs
@@ -126,6 +126,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
+        FindObjectOfType<LevelController>().LoadGameOver();
         Destroy(this.gameObject);
         AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
     }
