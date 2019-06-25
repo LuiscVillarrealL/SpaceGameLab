353895eec9230b3d1b884b339619b76609fb75dc
luiscvillarreall <luiscvillarreall@gmail.com>
2019-06-25 11:41:52 -0500

    siguen problemas con camara

--- EspacioLab/Assets/Scripts/ScoreDisplay.cs
+++ EspacioLab/Assets/Scripts/ScoreDisplay.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class ScoreDisplay : MonoBehaviour
 {
 
-    Text scoreText;
+    TMPro.TextMeshProUGUI scoreText;
+
+    
     Score score;
 
     // Start is called before the first frame update
     void Start()
     {
-        scoreText = GetComponent<Text>();
+        scoreText = GetComponent<TMPro.TextMeshProUGUI>();
         score = FindObjectOfType<Score>();
     }
 
